fad5e455604a56b4fc940c60962ab50f20fdda38
Mr.Web <lombardi.nd@gmail.com>
2026-01-23 12:21:38 +0100

    Bugfix

---
+++ lang/it/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'La password è stata reimpostata.',
+    'sent' => 'Ti abbiamo inviato il link per reimpostare la password.',
+    'throttled' => 'Attendi prima di riprovare.',
+    'token' => 'Il token di reimpostazione della password non è valido.',
+    'user' => 'Non esiste un utente con questo indirizzo email.',
+];
