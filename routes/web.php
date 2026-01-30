cf8b3929fcc5143a831de9ec34599389e6f18544
Mr.Web <lombardi.nd@gmail.com>
2026-01-30 09:43:03 +0100

    404 fix

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/admin');
+Route::redirect('/', 'https://www.simecom.it/');
 
 // Landing page video personalizzato
 Route::get('/v/{uuid}', [LandingController::class, 'show'])->name('video.landing');
